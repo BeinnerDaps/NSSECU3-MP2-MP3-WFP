46410ce48e50fffc6a6ed014f3374b0244320a54
BienMags <bienmagdamo2021@gmail.com>
2024-06-11 00:57:49 +0800

    Update odlParser.py and added sample odl.csv

--- odlParser.py
+++ odlParser.py
@@ -1,53 +1,34 @@
-import csv
 import pandas as pd
 import argparse
 
-def getColumns(file):
+def readCSV(file):
     df = pd.read_csv(file)
-    fileName = df['Filename'].tolist()
-    fileIndex = df['File_Index'].tolist()
-    timeStamp = df['Timestamp'].tolist()
-    codeFile = df['Code_File'].tolist()
-    function = df['Function'].tolist()
+    df.dropna(how="all").reset_index(drop=True)
+    df.drop(['File_Index'], axis=1, inplace=True)
+    return parseDatetime(df.copy())
 
-    pfileName = parseFile(fileName)
-    ptimeStamp = parseTimeStamp(timeStamp)
-    pcodeFile = parseCodeFile(codeFile)
-    pfunction = parseFunction(function)
+def parseDatetime(df_dt):
+    df_dt['Timestamp'] = df_dt['Timestamp'].str.split('.').str[0]
+    print(df_dt['Timestamp'].head(20))
+    return parseCodeFile(df_dt.copy())
 
-    writeCSV(pfileName, fileIndex, ptimeStamp, pcodeFile, pfunction)
-
-def parseFile(fileName):
+def parseCodeFile(df_codefile):
     pass
+    return parseFunction(df_codefile.copy())
 
-def parseTimeStamp(timeStamp):
+def parseFunction(df_function):
     pass
+    return writeCSV(df_function.copy())
 
-def parseCodeFile(codeFile):
+def writeCSV(df):
+
     pass
-
-def parseFunction(function):
-    pass
-
-def writeCSV(pfileName, pfileIndex, ptimeStamp, pcodeFile, pfunction):
-    if len(pfileName) == len(pfileIndex) == len(ptimeStamp) == len(pcodeFile) == len(pfunction):
-        rows = zip(pfileName, pfileIndex, ptimeStamp, pcodeFile, pfunction)
-        header = ["Filename", "File_Index", "Timestamp", "Code_File", "Function"]
-
-        with open('Parsed_ODL_Report.csv', mode='w', newline='') as newFile:
-            writer = csv.writer(newFile)
-            writer.writerow(header)
-            writer.writerows(rows)
-
-        print("CSV file created successfully.")
-    else:
-        print("Error: All lists must be of the same length.")
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('file', type=float, help='The first argument.')
+    parser.add_argument('file', type=str, help='CSV file')
     args = parser.parse_args()
-    getColumns(args.file)
+    readCSV(args.file)
 
 if __name__ == '__main__':
     main()
