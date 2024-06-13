401a4aa5892213ad2218f89d1f8fe6ee5f98f81f
BienMags <bienmagdamo2021@gmail.com>
2024-06-13 19:24:47 +0800

    Updated RBOdlCmd.py

---
+++ MP2/RBOdlCmd.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+import os
+import sys
+import pandas as pd
+import tempfile
+
+def run_tool(tool, path, output_path, obfuscationstringmap_path, all_key_values, all_data):
+    if tool == 'odl':
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".csv") as temp_csv:
+            output_file = temp_csv.name
+        commands = [['python', 'odl.py', path, '-o', output_file]]
+        if obfuscationstringmap_path:
+            commands[0].extend(['-s', obfuscationstringmap_path])
+        if all_key_values:
+            commands[0].append('-k')
+        if all_data:
+            commands[0].append('-d')
+    elif tool == 'rb':
+        new_folder = os.path.join(output_path, 'RBCMetaData')
+        try:
+            os.makedirs(new_folder)
+            print(f"Directory {new_folder} created successfully.")
+        except OSError as error:
+            print(f"Error creating directory {new_folder}: {error}")
+
+        commands = [
+            ['cmd', '/c', f'cd {path}'],
+            ['cmd', '/c', f'copy {os.path.join(path, "$I*")} {new_folder}'],
+            ['RBCmd.exe', '-d', new_folder, '--csv', output_path]
+        ]
+
+    print("Running commands:")
+    for cmd in commands:
+       if cmd: print(' '.join(cmd))
+    runParsers(commands)
+
+    if tool == 'odl':
+        readCSV(output_file, output_path)
+
+def runParsers(commands):
+    try:
+        for command in commands:
+            subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running Odl.py: {e}")
+
+def readCSV(file, output_path):
+    try:
+        df = pd.read_csv(file)
+        df.dropna(how="all", inplace=True)
+        df.reset_index(drop=True, inplace=True)
+        df.drop(['File_Index'], axis=1, inplace=True)
+        return parseDatetime(df.copy(), output_path)
+    except PermissionError as e:
+        print(f"Permission Denied: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def parseDatetime(df_dt, output_path):
+    df_dt['Timestamp'] = df_dt['Timestamp'].str.split('.').str[0]
+    return parseFunction(df_dt.copy(), output_path)
+
+def parseFunction(df_func, output_path):
+    df_func['Function'] = df_func['Function'].str.replace(r'(?<!^)(?=[A-Z])', ' ', regex=True).str.replace('::', ' -')
+    return writeCSV(df_func.copy(), output_path)
+
+def writeCSV(df, output_path):
+    output = os.path.join(output_path, 'output.xlsx')
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Parsed')
+        worksheet = writer.sheets['Parsed']
+        for col in df:
+            colWidth = max(df[col].astype(str).map(len).max(), len(col))
+            colWidth = 100 if colWidth > 100 else colWidth
+            colIndex = df.columns.get_loc(col)
+            worksheet.set_column(colIndex, colIndex, colWidth)
+    print(f"DataFrame has been written to {output}")
+
+def main():
+    parser = argparse.ArgumentParser(description="Wrapper script to run odl.py or RBCmd.exe")
+    parser.add_argument('tool', choices=['odl', 'rb'], help='Specify which tool to run: odl (odl.py) or rb (RBCmd.exe)')
+    parser.add_argument('path', help='Path to folder with .odl logs for odl or Path to recycle bin with $I files for rb')
+    parser.add_argument('-o', '--output_path', help='Path to output', default=None)
+    parser.add_argument('-s', '--obfuscationstringmap_path', help='Path to ObfuscationStringMap.txt (if not in odl_folder)', default=None)
+    parser.add_argument('-k', '--all_key_values', action='store_true', help='For repeated keys in ObfuscationMap, get all values | delimited (off by default)')
+    parser.add_argument('-d', '--all_data', action='store_true', help='Show all data (off by default)')
+    args = parser.parse_args()
+
+    if args.output_path is None:
+        output_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+    run_tool(args.tool, args.path, output_path, args.obfuscationstringmap_path, args.all_key_values, args.all_data)
+
+if __name__ == "__main__":
+    main()
---
+++ MP2/run_commands.bat
@@ -0,0 +1,6 @@
+@echo off
+                        cd C:\.Bin>cd S-1-5-21-24768837-1461444044-554365501-1001
+                        copy C:\.Bin>cd S-1-5-21-24768837-1461444044-554365501-1001\$I* C:\Users\Bien Rafael\OneDrive\Documents\De Lasalle University\3rd year - TERM 3\NSSECU3\NSSECU3_Git\MP2\RBCMetaData
+                        RBCmd.exe -d C:\Users\Bien Rafael\OneDrive\Documents\De Lasalle University\3rd year - TERM 3\NSSECU3\NSSECU3_Git\MP2\RBCMetaData --csv C:\Users\Bien Rafael\OneDrive\Documents\De Lasalle University\3rd year - TERM 3\NSSECU3\NSSECU3_Git\MP2
+                        pause
+                        
